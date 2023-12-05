f0ad09a18d45fc0ef4457b8597175d174879bba8
vierghan <zavierghaniy@gmail.com>
2023-12-05 22:07:59 +0700

    add to master

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function hitungKeliling() {
+    var sisiA = parseFloat(document.getElementById("sisiA").value);
+    var sisiB = parseFloat(document.getElementById("sisiB").value);
+    var sisiC = parseFloat(document.getElementById("sisiC").value);
+    var keliling = sisiA + sisiB + sisiC;
+    document.getElementById("hasil").value = keliling + " cm";
+}
+
+function hitungLuas() {
+    var alas = parseFloat(document.getElementById("alas").value);
+    var tinggi = parseFloat(document.getElementById("tinggi").value);
+    var luas = 0.5 * alas * tinggi;
+    document.getElementById("hasil").value = luas + " cm";
+}
